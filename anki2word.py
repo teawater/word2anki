afa833de6b54ca488afb88251f640e840b6f9f02
teawater <teawater@gmail.com>
2025-04-01 19:53:05 +0800

    anki2word.py: Change to python3
    Signed-off-by: teawater <teawater@gmail.com>

--- anki2word.py
+++ anki2word.py
@@ -1,46 +1,35 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import pypinyin
-import optparse
-import urllib
-import urllib2
+import argparse
+import urllib.request
 import re
-import time
 
-parser = optparse.OptionParser()
-parser.add_option("-a", "--anki", dest="anki",
-                  help="anki txt file")
-parser.add_option("-o", "--out", action="store",
-                  type="string", default="./word.txt")
-args = parser.parse_args()[0]
-
-if args.anki == None:
-    parser.error("option --anki must set")
-
-def ensure_unicode(v):
-    if isinstance(v, str):
-        v = v.decode('utf8')
-    return unicode(v)
+parser = argparse.ArgumentParser()
+parser.add_argument("-a", "--anki", dest="anki",
+                    help="anki txt file", required=True)
+parser.add_argument("-o", "--out", action="store",
+                    type=str, default="./word.txt")
+args = parser.parse_args()
 
 fail_lines = []
-out = open(args.out, "w")
-for raw_line in open(args.anki):
-    raw_line = raw_line.strip()
-    if raw_line == "" or raw_line[0] == '#':
-        continue
+with open(args.out, "w", encoding='utf-8') as out:
+    with open(args.anki, encoding='utf-8') as anki_file:
+        for raw_line in anki_file:
+            raw_line = raw_line.strip()
+            if raw_line == "" or raw_line[0] == '#':
+                continue
 
-    searchObj = re.search(r'\t\"(\W+)\<div\>', raw_line)
-    if searchObj == None:
-        fail_lines.append(raw_line)
-        continue
+            searchObj = re.search(r'\t\"(\W+)\<div\>', raw_line)
+            if searchObj is None:
+                fail_lines.append(raw_line)
+                continue
 
-    out.write(searchObj.group(1))
-    out.write("\n")
+            out.write(searchObj.group(1))
+            out.write("\n")
 
-out.close()
-if len(fail_lines) > 0:
-    print "这些行处理失败"
+if fail_lines:
+    print("这些行处理失败")
     for line in fail_lines:
-        print line
-
+        print(line)
