d877bb754d3130d906a501fdb00abff3a3ef2822
teawater <teawater@gmail.com>
2025-04-01 20:08:47 +0800

    getgif.py: Change to python3
    Signed-off-by: teawater <teawater@gmail.com>

--- getgif.py
+++ getgif.py
@@ -1,46 +1,40 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import optparse
+import argparse
 import os
-import urllib
-import urllib2
+import urllib.request
+import urllib.parse
 import re
 import random
 import time
 
-parser = optparse.OptionParser()
-parser.add_option("-w", "--word", dest="word",
-                  help="word file")
-parser.add_option("-o", "--out", action="store",
-                  type="string", default="./out")
-parser.add_option("-d", "--old", action="store",
-                  type="string", default="./old")
-parser.add_option("-c", "--cn", action="store_true", default=False)
-args = parser.parse_args()[0]
+parser = argparse.ArgumentParser()
+parser.add_argument("-w", "--word", dest="word",
+                    help="word file", required=True)
+parser.add_argument("-o", "--out", action="store",
+                    type=str, default="./out")
+parser.add_argument("-d", "--old", action="store",
+                    type=str, default="./old")
+parser.add_argument("-c", "--cn", action="store_true", default=False)
+args = parser.parse_args()
 
-if args.word == None:
-    parser.error("option --word must set")
 if not os.path.exists(args.out):
     os.makedirs(args.out)
 if not os.path.exists(args.old):
-    print args.old + " is not exist"
+    print(args.old + " is not exist")
     args.old = ""
 elif os.path.isfile(args.out):
     parser.error("option --out should set a directory")
 
-print "从文件", args.word, "读入词语"
-print "文件存入目录", args.out
+print("从文件", args.word, "读入词语")
+print("文件存入目录", args.out)
 
-def ensure_unicode(v):
-    if isinstance(v, str):
-        v = v.decode('utf8')
-    return unicode(v)
 
 def download_file(url, filename):
     try:
-        local_filename, headers = urllib.urlretrieve(url, filename)
-        if headers.gettype() != 'image/gif':
+        local_filename, headers = urllib.request.urlretrieve(url, filename)
+        if headers.get_content_type() != 'image/gif':
             return False
 
         content_length = headers.get('Content-Length')
@@ -55,58 +49,60 @@ def download_file(url, filename):
 
 
 def get_from_strokeorder(word, pic_dir):
-    utf8_word = word.encode('utf8')
-    url_word = urllib.quote(utf8_word)
+    url_word = urllib.parse.quote(word)
 
     try:
         if args.cn:
-            contents = urllib2.urlopen("http://bishun.strokeorder.info/mandarin.php?q="+url_word).read()
+            contents = urllib.request.urlopen("http://bishun.strokeorder.info/mandarin.php?q=" + url_word).read().decode('utf-8')
         else:
-            contents = urllib2.urlopen("http://www.strokeorder.info/mandarin.php?q="+url_word).read()
-    except:
+            contents = urllib.request.urlopen("http://www.strokeorder.info/mandarin.php?q=" + url_word).read().decode('utf-8')
+    except Exception as e:
+        print("Error occurred while fetching stroke order: {}".format(e))
         return False
+
     if args.cn:
-        searchObj = re.search(r'\<img src=\"(http:\/\/bishun\.strokeorder\.info\/characters\/\d+\.gif)\" alt\=\"' + utf8_word + r'的笔顺\"\>', contents)
+        searchObj = re.search(r'<img src="(http://bishun.strokeorder.info/characters/\d+\.gif)" alt="' + word + r'的笔顺">', contents)
     else:
-        searchObj = re.search(r'\<img src=\"(http:\/\/bishun\.strokeorder\.info\/characters\/\d+\.gif)\" alt\=\"stroke order animation of ' + utf8_word + r'\"\>', contents)
-    if searchObj == None:
+        searchObj = re.search(r'<img src="(http://bishun.strokeorder.info/characters/\d+\.gif)" alt="stroke order animation of ' + word + r'">', contents)
+    if searchObj is None:
         return False
     pic_url = searchObj.group(1)
     return download_file(pic_url, pic_dir)
+
 
 def get_from_zdic(word, pic_dir):
     unicode_word = hex(ord(word))[2:].upper().rstrip('L').zfill(4)
     return download_file("https://img.zdic.net/kai/jbh/" + unicode_word + ".gif", pic_dir)
 
+
 fail_words = []
-for line in open(args.word):
-    line = line.strip()
-    if line == "" or line[0] == '#':
-        continue
-    line = ensure_unicode(line)
-    for word in line:
-        print "尝试取得", word, "的图片"
-        unicode_word = hex(ord(word))[2:].upper().rstrip('L').zfill(4)
-        pic_dir = os.path.join(args.out, word + ".gif")
-
-        if os.path.exists(pic_dir):
-            print word, "的文件已经存在"
+with open(args.word, encoding='utf-8') as word_file:
+    for line in word_file:
+        line = line.strip()
+        if line == "" or line[0] == '#':
             continue
+        for word in line:
+            print("尝试取得", word, "的图片")
+            pic_dir = os.path.join(args.out, word + ".gif")
 
-        if args.old != "" and os.path.exists(os.path.join(args.old, word + ".gif")):
-            print word, "的文件已经存在" + args.old
-            continue
-
-        if not get_from_strokeorder(word, pic_dir):
-            if not get_from_zdic(word, pic_dir):
-                fail_words.append(word)
-                print word, "下载失败"
+            if os.path.exists(pic_dir):
+                print(word, "的文件已经存在")
                 continue
 
-        print pic_dir
-        time.sleep(random.randint(1,3))
+            if args.old != "" and os.path.exists(os.path.join(args.old, word + ".gif")):
+                print(word, "的文件已经存在", args.old)
+                continue
+
+            if not get_from_strokeorder(word, pic_dir):
+                if not get_from_zdic(word, pic_dir):
+                    fail_words.append(word)
+                    print(word, "下载失败")
+                    continue
+
+            print(pic_dir)
+            time.sleep(random.randint(1, 3))
 
 if len(fail_words) > 0:
-    print "这些字下载失败"
+    print("这些字下载失败")
     for w in fail_words:
-        print w
+        print(w)
